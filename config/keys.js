e92a1a40471a6807f946acc79ee809b907362147
Michael LeGrange <mlegrange15@att.net>
2018-10-10 06:02:57 -0500

    added package.json server config model and routes

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://mlegrange15:iloAsic15!m@ds227373.mlab.com:27373/mern-cart'
+}
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const items = require('./routes/api/items')
+
+const app = express();
+
+//Middleware
+app.use(bodyParser.json());
+
+// DB config
+const db = require ('./config/keys').mongoURI;
+
+// Mongo Connection
+mongoose.connect(db)
+    .then(() => console.log('MongoDB is Connected!'))
+    .catch( error => console.log(error));
+
+    const port = process.env.PORT || 5000;
+
+    app.listen (port, () => console.log(`Server started on port ${port}`));
+
